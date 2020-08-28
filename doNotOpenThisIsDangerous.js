e312d94af6102b111430f1f700736e2e6f9605fb
Bullz04 <62468250+Bullz04@users.noreply.github.com>
2020-08-28 09:54:19 +0700

    Add files via upload

---
+++ doNotOpenThisIsDangerous.js
@@ -0,0 +1,160 @@
+/*
+wHVWDJUVVWVWQjuvbgvbwj;dug wu;dGKJQgv b;dAS WQjausd wqj Jkwudn ksj
+wefef
+fr
+qrf
+qrWFVWKifchecfhkjE  CVHKwudsvchK    QNVW    rvRW    
+GVQRWgvrqwhFCVKWQHVFQRWN GVQJKhn lwqhnv wqVF 
+RWVPIQRWHIKOVFHWDOLCNFH EKLCFH  QECFJK  EGRIJNBHQGB
+WV  WJHFV   KIBGVO  FWNVI   FV  
+V   RJ  RF  R
+F   H2KF    FJH Q
+    AAHxnlANC oldq;lehncfAWJLNCBFKJASWBVUJWKNVJKWVB
+    KJVBKAWDSJBVVHOIVHjbsckjsdcvdsVDC
+    Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+    
+*/
+function theScrollOfTruth() {
+/*
+ Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    oiewhCeiubbdebdbJKDBJKWVJWBVWV
+    WVKWQRGQRG
+    Q498RY894QYR8QH2G31G
+    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
+    F   wklvnwDKVN  kwnvlvvd    
+    WDV WDKj    wvb kvw  Ds"v: Ds"v:
+    oiewhCeiubbdebdbJKDBJKWVJWBVWV
+    WVKWQRGQRG
+    Q498RY894QYR8QH2G31G Ds"v:
+    oiewhCeiubbdebdbJKDBJKWVJWBVWV
+    WVKWQRGQRG
+    Q498RY894QYR8QH2G31G
+    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
+    F   wklvnwDKVN  kwnvlvvd    
+    WDV WDKj    wvb kvw 
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
+    F   wklvnwDKVN  kwnvlvvd    
+    WDV WDKj    wvb kvw 
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    oiewhCeiubbdebdbJKDBJKWVJWBVWV
+    WVKWQRGQRG
+    Q498RY894QYR8QH2G31G
+    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
+    F   wklvnwDKVN  kwnvlvvd    
+    WDV WDKj    wvb kvw 
+     Ds"v: Ds"v:
+    oiewhCeiubbdebdbJKDBJKWVJWBVWV
+    WVKWQRGQRG
+    Q498RY894QYR8QH2G31G
+    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
+    F   wklvnwDKVN  kwnvlvvd    
+    WDV WDKj    wvb kvw 
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    v"lfvBVNJKVBDaNWvdhk
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
+    DV;nvL;nVNB Ds"v:
+    oiewhCeiubbdebdbJKDBJKWVJWBVWV
+    WVKWQRGQRG
+    Q498RY894QYR8QH2G31G
+    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
+    F   wklvnwDKVN  kwnvlvvd    
+    WDV WDKj    wvb kvw 
+     Ds"v:
+    v"lfvBVNJKVBDaNWvdhk
+    
+*/
+console.log(
+    "Those top 3 players on Scrap 2 are Trash"
+)
+
+
+}
+
+function bullz_sucks() {
+    console.error("No u");
+}
+
+function no_u() {
+    console.error("Uno Reverse Card");
+}
+
+function bullz_is_trash() {
+    if (confirm("Do you think I'm trash?")) {
+        alert("HAHA YOUR SAVE IS GONE THEN >:D >:D");
+        console.error("Your save was deleted due to invalid variables.")
+        hardReset();
+    }
+}
