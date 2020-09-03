591f02b843944445e83901e6d6f8d9987708ca88
Bullz04 <62468250+Bullz04@users.noreply.github.com>
2020-09-03 11:18:48 +0700

    Update doNotOpenThisIsDangerous.js

--- doNotOpenThisIsDangerous.js
+++ doNotOpenThisIsDangerous.js
@@ -1,143 +1,7 @@
-/*
-wHVWDJUVVWVWQjuvbgvbwj;dug wu;dGKJQgv b;dAS WQjausd wqj Jkwudn ksj
-wefef
-fr
-qrf
-qrWFVWKifchecfhkjE  CVHKwudsvchK    QNVW    rvRW    
-GVQRWgvrqwhFCVKWQHVFQRWN GVQJKhn lwqhnv wqVF 
-RWVPIQRWHIKOVFHWDOLCNFH EKLCFH  QECFJK  EGRIJNBHQGB
-WV  WJHFV   KIBGVO  FWNVI   FV  
-V   RJ  RF  R
-F   H2KF    FJH Q
-    AAHxnlANC oldq;lehncfAWJLNCBFKJASWBVUJWKNVJKWVB
-    KJVBKAWDSJBVVHOIVHjbsckjsdcvdsVDC
-    Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-    
-*/
+
 function theScrollOfTruth() {
-/*
- Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    oiewhCeiubbdebdbJKDBJKWVJWBVWV
-    WVKWQRGQRG
-    Q498RY894QYR8QH2G31G
-    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
-    F   wklvnwDKVN  kwnvlvvd    
-    WDV WDKj    wvb kvw  Ds"v: Ds"v:
-    oiewhCeiubbdebdbJKDBJKWVJWBVWV
-    WVKWQRGQRG
-    Q498RY894QYR8QH2G31G Ds"v:
-    oiewhCeiubbdebdbJKDBJKWVJWBVWV
-    WVKWQRGQRG
-    Q498RY894QYR8QH2G31G
-    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
-    F   wklvnwDKVN  kwnvlvvd    
-    WDV WDKj    wvb kvw 
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
-    F   wklvnwDKVN  kwnvlvvd    
-    WDV WDKj    wvb kvw 
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    oiewhCeiubbdebdbJKDBJKWVJWBVWV
-    WVKWQRGQRG
-    Q498RY894QYR8QH2G31G
-    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
-    F   wklvnwDKVN  kwnvlvvd    
-    WDV WDKj    wvb kvw 
-     Ds"v: Ds"v:
-    oiewhCeiubbdebdbJKDBJKWVJWBVWV
-    WVKWQRGQRG
-    Q498RY894QYR8QH2G31G
-    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
-    F   wklvnwDKVN  kwnvlvvd    
-    WDV WDKj    wvb kvw 
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    v"lfvBVNJKVBDaNWvdhk
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    vbWL;SDv;Ksfawhv slawfjkv bhvfjb ;HG:VJkh SKAVNANLfw/
-    DV;nvL;nVNB Ds"v:
-    oiewhCeiubbdebdbJKDBJKWVJWBVWV
-    WVKWQRGQRG
-    Q498RY894QYR8QH2G31G
-    WGOI    HGIIOJE L   kjndjkkfbwkfw   ew  vf  W
-    F   wklvnwDKVN  kwnvlvvd    
-    WDV WDKj    wvb kvw 
-     Ds"v:
-    v"lfvBVNJKVBDaNWvdhk
-    
-*/
 console.log(
-    "Those top 3 players on Scrap 2 are Trash"
+    "Speed merging is disallowed.<br>I want to cap merge boosts."
 )
 
 
@@ -154,7 +18,7 @@ function no_u() {
 function bullz_is_trash() {
     if (confirm("Do you think I'm trash?")) {
         alert("HAHA YOUR SAVE IS GONE THEN >:D >:D");
-        console.error("Your save was deleted due to invalid variables.")
+        console.error("Your save was deleted due to invalid variables to prevent any computer damages.")
         hardReset();
     }
 }
